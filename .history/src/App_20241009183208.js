6c7169567d7f5ae3e4afc0c20a5c8d825b3de5ac
Renan Reis <rrgdosreis@gmail.com>
2024-10-10 15:11:38 -0300

    Atualização 10/10/2024

---
+++ .history/src/App_20241009183208.js
@@ -0,0 +1,420 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
+//logomarca
+import LogoImage from './img/logo.png';
+import LogoImagee from './img/logoo.png';
+// Ícones do layout
+import { TbDeviceDesktopAnalytics } from "react-icons/tb";
+import { IoShieldCheckmark } from "react-icons/io5";
+import { MdOutlineContactMail } from "react-icons/md";
+import { FaCar, FaChartLine, FaTasks, FaInstagram, FaWhatsapp, FaLinkedin } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
+import { TbMapDollar } from "react-icons/tb";
+import { HiHome } from "react-icons/hi";
+import { MdDesignServices } from "react-icons/md";
+import { MdAssuredWorkload } from "react-icons/md";
+import { MdOutlineAttachEmail } from "react-icons/md";
+import { Drawer } from '@mui/material';
+import { HiLockClosed } from "react-icons/hi";
+import { IoLogoWhatsapp } from "react-icons/io";
+// Efeito de animação
+import { motion } from 'framer-motion';
+import { Typewriter } from 'react-simple-typewriter';
+// Estilos
+import {
+  Container,
+  Box,
+  TextDefault,
+  Header,
+  NavBar,
+  HeaderWrapper,
+  WelcomeTextWrapper,
+  WelcomeText,
+  BackgroundImage,
+  LogoImg,
+  HeroSection,
+  ServicesSection,
+  ServiceCard,
+  Footer,
+  FleetSection,
+  FleetCard,
+  LinkMenu
+} from './stylesGlobal';
+
+const App = () => {
+
+  const [scrolled, setScrolled] = useState(false);
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [logoFooter, setLogoFooter] = useState(false);
+
+  const [areaDesktop, setAreaDesktop] = useState(false);
+  const [areaMobile, setAreaMobile] = useState(false);
+
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const handleScroll = () => {
+    const isScrolled = window.scrollY > 50; // Muda a cor após 50px de rolagem
+    setScrolled(isScrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+
+    if (isMobile) {
+      setAreaMobile(true);
+      setAreaDesktop(false);
+      setLogoFooter(false);
+    } else {
+      setAreaMobile(false);
+      setAreaDesktop(true);
+      setLogoFooter(true);
+    }
+
+  }, [isMobile]);
+
+
+  return (
+    <Container>
+
+      {/* Background com Parallax e Zoom */}
+      <BackgroundImage />
+
+      {/* Header fixo */}
+      <HeaderWrapper scrolled={scrolled}>
+        <LogoImg left={'30px'} src={LogoImage} alt="Logo da Fleet Solutions" />
+
+        {areaDesktop &&
+          <NavBar style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+            <a href="#home" style={{ display: 'flex', alignItems: 'center' }}>
+              <HiHome style={{ marginRight: '8px' }} /> Início
+            </a>
+            <a href="#services" style={{ display: 'flex', alignItems: 'center' }}>
+              <MdDesignServices style={{ marginRight: '8px' }} /> Serviços
+            </a>
+            <a href="#fleet" style={{ display: 'flex', alignItems: 'center' }}>
+              <MdAssuredWorkload style={{ marginRight: '8px' }} /> Fleet Solutions
+            </a>
+            <a href="#contact" style={{ display: 'flex', alignItems: 'center' }}>
+              <FaWhatsapp style={{ marginRight: '8px' }} /> Contato
+            </a>
+            <motion.button
+              style={{
+                backgroundColor: '#FF4500',
+                color: '#FFFFFF',
+                border: 'none',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                marginLeft: 'auto',
+                marginRight: '50px',
+              }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.3 }}
+            >
+              Processos
+            </motion.button>
+          </NavBar>
+        }
+
+        {areaMobile && (
+          <>
+            {/* Ícone de menu para abrir o Drawer */}
+            <motion.div
+              style={{ cursor: 'pointer', top: '20px', left: '20px', marginRight: '20px' }}
+              onClick={toggleDrawer}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.3 }}
+            >
+              <FaBars size={30} />
+            </motion.div>
+
+            <Drawer
+              anchor="right"
+              open={drawerOpen}
+              onClose={toggleDrawer}
+              PaperProps={{
+                style: { width: '40%', padding: '10px', backgroundColor: '#FF4500' },
+              }}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  padding: '20px',
+                  height: '100%',
+                  alignItems: 'flex-start',
+                  backgroundColor: 'white',
+                }}
+                role="presentation"
+                onClick={toggleDrawer}
+                onKeyDown={toggleDrawer}
+              >
+                <nav style={{ display: 'flex', height: '70%', width: '100%', alignItems: 'flex-end', flexDirection: 'column' }}>
+                  <LinkMenu href="#home" >
+                    <LogoImg src={LogoImagee} alt="Logo da Fleet Solutions" />
+                  </LinkMenu>
+
+                  <LinkMenu href="#home" >
+                    <TextDefault color="#FFFFFF" size="19px">Início <HiHome style={{ marginRight: '8px' }} /></TextDefault>
+                  </LinkMenu>
+
+                  <LinkMenu href="#services">
+                    <TextDefault color="#FFFFFF" size="19px">Serviços <MdDesignServices style={{ marginRight: '8px' }} /></TextDefault>
+                  </LinkMenu>
+
+                  <LinkMenu href="#fleet">
+                    <TextDefault color="#FFFFFF" size="19px">Fleet Solutions <MdAssuredWorkload style={{ marginRight: '8px' }} /></TextDefault>
+                  </LinkMenu>
+
+                  <LinkMenu href="#contact">
+                    <TextDefault color="#FFFFFF" size="19px">Contato <IoLogoWhatsapp style={{ marginRight: '8px' }} /></TextDefault>
+                  </LinkMenu>
+
+                  <LinkMenu href="#acess">
+                    <TextDefault weight={'bold'} color="#FFFFFF" size="17px">Meu acesso <HiLockClosed size={'19px'} style={{ marginRight: '8px' }} /></TextDefault>
+                  </LinkMenu>
+
+                </nav>
+              </Box>
+            </Drawer>
+          </>
+        )}
+      </HeaderWrapper>
+
+      <WelcomeTextWrapper>
+        
+          <TextDefault size={'33px'} color={'#FF4500'} weight={'bold'} bottom={'10px'}>Fleet Solutions</TextDefault>
+          <TextDefault size={'22px'} color={'#FFFFFF'} top={'10px'} bottom={'10px'}><IoShieldCheckmark size="25px" /> Defesa de Multas</TextDefault>
+          <TextDefault size={'22px'} color={'#FFFFFF'} top={'10px'} bottom={'10px'}><MdOutlineContactMail size="25px" /> Suspensão de CNH</TextDefault>
+          <TextDefault size={'22px'} color={'#FFFFFF'} top={'10px'} bottom={'10px'}><TbDeviceDesktopAnalytics size="27px" /> Gestão de Frotas</TextDefault>
+          <TextDefault size={'22px'} color={'#FFFFFF'} top={'10px'} bottom={'10px'}><IoShieldCheckmark size="25px" /> Cassação de CNHSoluções</TextDefault>
+          <TextDefault size={'22px'} color={'#FFFFFF'} top={'10px'} bottom={'10px'}><IoShieldCheckmark size="25px" /> Soluções Personalizadas para empresas</TextDefault>
+          <TextDefault size={'22px'} color={'#FFFFFF'} top={'10px'} bottom={'10px'}><IoShieldCheckmark size="25px" /> Defesa de Multas</TextDefault>
+
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.3 }}
+            style={{
+              backgroundColor: '#FF4500',
+              color: '#FFFFFF',
+              border: 'none',
+              padding: '10px 20px',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              marginLeft: '30px',
+            }}
+          >
+            <TextDefault>Saiba Mais</TextDefault>
+            <FaWhatsapp style={{ marginRight: '8px' }} />
+
+          </motion.button>
+
+
+
+        
+      </WelcomeTextWrapper>
+
+      {/* Seção de Serviços */}
+      <ServicesSection id="services">
+        <LogoImg src={LogoImagee} alt="Logo da Fleet Solutions" />
+
+        <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          {/** Aguarda 3 segundos antes de iniciar o efeito de digitação */}
+          <Typewriter
+            words={['Como podemos te ajudar?']}
+            loop={3}
+            cursor
+            cursorStyle="_"
+            typeSpeed={100}
+            deleteSpeed={100}
+            delaySpeed={5000}
+            onInit={(typewriter) => {
+              setTimeout(() => {
+                typewriter.start(); // Inicia o efeito de digitação após 3 segundos
+              }, 3000); // Delay de 3 segundos
+            }}
+          />
+        </motion.h1>
+
+
+        <div className="cards" style={{ marginTop: '70px' }}>
+
+          <ServiceCard>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.3 }}
+            >
+              <h3><IoShieldCheckmark size="25px" /> Defesa de Multas</h3>
+              <p>Oferecemos suporte completo para defesa de multas.</p>
+            </motion.div>
+          </ServiceCard>
+
+          <ServiceCard>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.3 }}
+            >
+              <h3> <MdOutlineContactMail size="25px" /> Suspensão de CNH</h3>
+              <p>Saiba como evitamos a suspensão da sua carteira de habilitação.</p>
+            </motion.div>
+          </ServiceCard>
+
+          <ServiceCard>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.3 }}
+            >
+              <h3><TbDeviceDesktopAnalytics size="27px" /> Gestão de Frotas</h3>
+              <p>Monitore seus motoristas, controle as revisões e gerencie sua frota com mais eficiência e tranquilidade.</p>
+            </motion.div>
+          </ServiceCard>
+        </div>
+      </ServicesSection>
+
+      {/* Hero Section */}
+      <HeroSection>
+
+        <TextDefault bottom={'20px'} color="#FFFFFF" size={isMobile ? '27px' : '35px'} left={"30px"} weight={'bold'}>Gestão de Frotas e Soluções para Motoristas</TextDefault>
+        <TextDefault color="#FFFFFF" size={isMobile ? '19px' : '24px'} left="30px">• Defesa de Multas, Suspensão de CNH, e mais!</TextDefault>
+        <TextDefault bottom={'20px'} color="#FFFFFF" size={isMobile ? '19px' : '24px'} left="30px">• Tenha um relatório completo na palma de sua mão.</TextDefault>
+
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.3 }}
+          style={{
+            backgroundColor: '#FF4500',
+            color: '#FFFFFF',
+            border: 'none',
+            padding: '10px 20px',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            marginLeft: '30px',
+          }}
+        >
+          Solicitar contato <FaWhatsapp style={{ marginRight: '8px' }} />
+        </motion.button>
+
+      </HeroSection>
+
+      <FleetSection id="fleet">
+
+        <LogoImg src={LogoImage} alt="Logo da Fleet Solutions" />
+
+        <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          Para quem é a Fleet Solutions?
+        </motion.h1>
+
+        <motion.p
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          style={{ marginRight: '13px', marginLeft: '13px' }}
+        >
+          Criada em 2024, a Fleet Solutions nasceu com a missão de oferecer
+          serviços de alta qualidade e soluções eficientes no gerenciamento
+          de frotas. Nossa empresa se dedica a proporcionar uma gestão
+          inteligente e eficaz, garantindo a otimização de recursos, redução
+          de custos e melhoria na operação dos nossos clientes. Acreditamos
+          na excelência do atendimento e no uso de tecnologia avançada para
+          trazer inovação e agilidade ao setor, sempre comprometidos com a
+          satisfação e sucesso das empresas que confiam em nossos serviços.
+        </motion.p>
+
+        <div className="fleet-cards" style={{ marginTop: '70px' }}>
+          <FleetCard>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.3 }}
+            >
+              <TbMapDollar size={49} /> {/* Ícone para Transportadoras */}
+              <h3>Transportadoras de Carga e Passageiros</h3>
+            </motion.div>
+          </FleetCard>
+
+          <FleetCard>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.3 }}
+            >
+              <FaCar size={49} /> {/* Ícone para Locadoras */}
+              <h3>Locadoras de Veículos</h3>
+            </motion.div>
+          </FleetCard>
+
+          <FleetCard>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.3 }}
+            >
+              <FaChartLine size={49} /> {/* Ícone para Despachantes */}
+              <h3>Despachantes</h3>
+            </motion.div>
+          </FleetCard>
+
+          <FleetCard>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.3 }}
+            >
+              <FaTasks size={49} /> {/* Ícone para Gestores de Frota */}
+              <h3>Gestores de Frota</h3>
+            </motion.div>
+          </FleetCard>
+        </div>
+      </FleetSection>
+
+      {/* Footer */}
+      <Footer id="contact">
+
+
+        {logoFooter &&
+          <LogoImg src={LogoImagee} alt="Logo da Fleet Solutions" />
+        }
+
+        <Box justify={'center'} align={'center'} direction={'column'}>
+          <TextDefault top={'5px'} color="#FFFFFF" size="13px" bottom={'5px'}>Contato:</TextDefault>
+        </Box>
+
+        <div className="social-icons">
+          <FaInstagram size={24} /> {/* Ícone do Instagram */}
+          <FaWhatsapp size={24} />  {/* Ícone do WhatsApp */}
+          <FaLinkedin size={24} />  {/* Ícone do LinkedIn */}
+        </div>
+
+        <TextDefault top={'15px'} color="#FFFFFF" size="17px" bottom={'5px'}><MdOutlineAttachEmail /> fleetsolutions@gmail.com</TextDefault>
+
+        <TextDefault top={'5px'} color="#FFFFFF" size="13px" >© 2024 Fleet Solutions. Todos os direitos reservados.</TextDefault>
+      </Footer>
+    </Container>
+  );
+};
+
+export default App;
---
+++ .history/src/stylesGlobal_20241009202317.js
@@ -0,0 +1,374 @@
+import styled, { createGlobalStyle, keyframes } from "styled-components";
+import backgroundImage from './img/ImageInitialllll.jpg';
+import backgroundImageWhite from './img/ImageInitiallll.jpg';
+
+// Animação de Zoom contínuo no fundo
+const zoomEffect = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+// Animação de Fade-In para o texto
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+export const Container = styled.div`
+  //display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-x: hidden;
+  //width: 100%;
+`;
+
+// Background com Zoom e Parallax
+export const BackgroundImage = styled.div`
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+  height: 80vh;
+  width: 100%;
+  overflow: hidden; // Garantir que a imagem não afete o scroll horizontal
+  position: relative;
+  z-index: -1; // A imagem de fundo ficará atrás dos textos
+  animation: ${zoomEffect} 15s infinite;
+`;
+
+// Header
+export const HeaderWrapper = styled.header`
+  background-color: ${(props) => (props.scrolled ? "rgba(0, 0, 0, 0.5)" : "transparent")};
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 10;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.5s ease; // Transição suave
+`;
+
+// NavBar com animação de hover
+export const NavBar = styled.nav`
+  display: flex;
+  gap: 20px;
+  margin-right:50px;
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+    position: relative;
+
+    &:hover {
+      color: #FF4500;
+      transition: color 0.3s ease;
+    }
+
+    &:hover::after {
+      content: '';
+      width: 100%;
+      height: 2px;
+      background-color: #FF4500;
+      position: absolute;
+      bottom: -5px;
+      left: 0;
+      animation: ${fadeIn} 0.3s ease;
+    }
+  }
+`;
+
+// Texto ao lado direito com fonte sofisticada e estática
+export const WelcomeTextWrapper = styled.div`
+  position: absolute;
+  left: 70px; // Texto ficará à esquerda
+  top: 25%;
+  z-index: 1; // Texto acima do fundo
+  max-width: 40%;
+  animation: ${fadeIn} 2.5s ease;
+`;
+
+export const WelcomeTextWrapperLogo = styled.div`
+  position: absolute;
+  right: 70px; // Texto ficará à esquerda
+  top: 25%;
+  z-index: 1; // Texto acima do fundo
+  max-width: 40%;
+  animation: ${fadeIn} 2.5s ease;
+`;
+
+
+
+export const WelcomeText = styled.div`
+  color: white;
+  font-family: 'Georgia', serif; // Fonte mais sofisticada
+  font-size: 1.5rem;
+
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 20px;
+    color: #fff;
+  }
+
+  p {
+    line-height: 1.5;
+    text-align: right;
+  }
+`;
+
+export const Box = styled.div`
+  display: flex;
+  flex: ${(props) => props.flex || '1'};
+  background-color: ${(props) => props.color || 'transparent'};
+  width: ${(props) => props.width || '100%'};
+  height: ${(props) => props.height || 'auto'};
+  justify-content: ${(props) => props.justify || 'flex-start'};
+  align-items: ${(props) => props.align || 'flex-start'};
+  flex-direction: ${(props) => props.direction || 'row'};
+  margin-bottom: ${(props) => props.bottomSpace || '0px'};
+  margin-left: ${(props) => props.leftSpace || '0px'};
+  border-radius: ${(props) => props.radius || '0px'};
+  //padding: 10px; /* Adiciona espaçamento interno */
+`;
+
+export const TextDefault = styled.h1`
+  font-size: ${(props) => props.size || '15px'};
+  color: ${(props) => props.color || '#000'};
+  font-weight: ${(props) => props.weight || 'normal'};
+  margin-left: ${(props) => props.left || '0px'};
+  margin-top: ${(props) => props.top || '0px'};
+  margin-right: ${(props) => props.right || '0px'};
+  margin-bottom: ${(props) => props.bottom || '0px'};
+`;
+
+export const AreaImgBackground = styled.div`
+  width: 100%;
+  height: 600px;
+  background-size: cover;
+  background-image: url(${(props) => props.currentImage});
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  @media (max-width: 768px) {
+    height: 600px; /* Ajusta a altura da imagem para dispositivos menores */
+  }
+`;
+
+export const LogoImg = styled.img`
+  width: ${(props) => props.left || '60px'};
+  height: auto;
+  margin-left: ${(props) => props.left || '0px'};
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export const LogoImgWhite = styled.img`
+  width: 60px;
+  height: auto;
+  margin-left: ${(props) => props.left || '0px'};
+  margin-top: 80px;
+  margin-bottom: 10px;
+`;
+
+export const ServicesSection = styled.section`
+  width: 100%;
+  min-height: 700px; /* Define uma altura mínima */
+  background-color: #FF4500;
+  text-align: center;
+  color: #FFFFFF;
+  padding: 20px 0; /* Adiciona padding para espaçamento vertical */
+
+  h2 {
+    font-size: 2.5rem;
+    margin-bottom: 40px;
+  }
+
+  .cards {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap; /* Permite que os cartões se ajustem em várias linhas */
+
+    @media (max-width: 768px) {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+  
+  @media (min-width: 1024px) {
+    height: auto; /* Altura flexível para telas maiores */
+    padding: 50px 0; /* Aumenta o padding para dar mais espaço */
+  }
+`;
+
+export const LinkMenu = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  height: 70px;
+  text-decoration: none;
+
+`;
+
+export const ServiceCard = styled.div`
+  background: white;
+  border-radius: 7px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  width: 25%;
+  text-align: center;
+  color: #000;
+  transition: transform 0.3s;
+
+  h3 {
+    font-size: 1.5rem;
+    margin-bottom: 20px;
+  }
+
+  p {
+    font-size: 1rem;
+    color: #555;
+  }
+
+  &:hover {
+    transform: translateY(-5px); /* Efeito de levantar ao passar o mouse */
+  }
+
+  @media (max-width: 768px) {
+    width: 80%; /* Ajusta a largura para dispositivos menores */
+    margin-bottom: 30px;
+  }
+`;
+
+export const HeroSection = styled.section`
+  width: 100%;
+  height: 500px;
+  background-size: cover;
+  background-image: url(${backgroundImageWhite});
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  button {
+    background-color: #f53629;
+    width: 230px;
+    color: white;
+    padding: 10px 20px;
+    font-size: 1rem;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+
+    &:hover {
+      background-color: #d72f23;
+      transform: translateY(-2px);
+    }
+  }
+`;
+
+export const Footer = styled.footer`
+  display: flex;
+  width: 100%;
+  height: 200px;
+  background-color: #333;
+  color: white;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0;
+
+  p {
+    margin: 2px;
+    font-size: 0.9rem;
+  }
+
+  @media (max-width: 768px) {
+    height: 150px;
+  }
+
+  .social-icons {
+    display: flex;
+    justify-content: center;
+    gap: 15px;
+
+    svg {
+      cursor: pointer;
+      transition: color 0.3s;
+
+      &:hover {
+        color: #ffcc00;
+      }
+    }
+  }
+`;
+
+export const FleetSection = styled.section`
+  width: 100%;
+  height: auto;
+  background-color: #f5f5f5;
+  text-align: center;
+  min-height: 600px;
+  padding: 50px 0;
+
+  h2 {
+    font-size: 2.5rem;
+    color: #333;
+    margin-bottom: 30px;
+  }
+
+  .fleet-cards {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 20px;
+
+    @media (max-width: 768px) {
+      flex-direction: column;
+      align-items: center;
+      height: auto;
+    }
+  }
+`;
+
+export const FleetCard = styled.div`
+  background: white;
+  border-radius: 7px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 20%;
+  text-align: center;
+
+  h3 {
+    font-size: 1.5rem;
+    color: #333;
+    margin-bottom: 15px;
+  }
+
+  p {
+    font-size: 1rem;
+    color: #555;
+  }
+
+  @media (max-width: 768px) {
+    width: 80%;
+    margin-bottom: 20px;
+  }
+`;
+
+
